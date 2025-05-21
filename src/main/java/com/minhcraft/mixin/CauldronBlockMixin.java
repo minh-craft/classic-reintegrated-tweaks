b63a7902a2dc053a76c7a53fafbd1fa1a4a1198d
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2025-05-21 15:23:30 -0700

    disable cauldrons making powder snow during snowy weather

---
+++ src/main/java/com/minhcraft/mixin/CauldronBlockMixin.java
@@ -0,0 +1,23 @@
+package com.minhcraft.mixin;
+
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.block.CauldronBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(CauldronBlock.class)
+public class CauldronBlockMixin {
+    @ModifyVariable(
+            method = "shouldHandlePrecipitation",
+            at = @At("HEAD"),
+            argsOnly = true
+    )
+    // change precipitation to none if it's snowing to disable powder snow generation in cauldrons
+    private static Biome.Precipitation modifyPrecipitation(Biome.Precipitation value) {
+        if (value == Biome.Precipitation.SNOW) {
+            return Biome.Precipitation.NONE;
+        }
+        return value;
+    }
+}
