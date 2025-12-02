6d48bea76f5f60f99c8e2cba2dfc64e04b405634
minhty-fresh <4746495+mdaole@users.noreply.github.com>
2025-12-02 11:22:54 -0800

    v0.13.0 - update ocelot to cat conversion code

--- src/main/java/com/minhcraft/mixin/entity/OcelotMixin.java
+++ src/main/java/com/minhcraft/mixin/entity/OcelotMixin.java
@@ -1,14 +1,9 @@
 package com.minhcraft.mixin.entity;
 
 import com.minhcraft.config.ModConfig;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.animal.Ocelot;
 import net.minecraft.world.entity.player.Player;
@@ -41,32 +36,23 @@ public class OcelotMixin {
         return 25.0;
     }
 
-    // Turn ocelot into cat when tamed
-    // Copied from https://github.com/Partonetrain/ocelotfix
-    // With a fix to prevent multiple cats from spawning from one tamed ocelot (injecting this code before sidedSuccess() instead of putting it at all returns)
-    @Inject(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/InteractionResult;sidedSuccess(Z)Lnet/minecraft/world/InteractionResult;"))
-    private void ocelotfix_spawnCat(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+    // Copy of Partonetrain's ocelot to cat conversion code from https://github.com/Partonetrain/trains_tweaks
+    // The code is an updated version of https://modrinth.com/mod/ocelotfix
+    @Inject(method = "mobInteract", at = @At("RETURN"))
+    private void trains_tweaks$mobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
         Ocelot ocelot = (Ocelot)(Object)this;
         if(ocelot.isTrusting())
         {
             Level level = ocelot.level();
 
             if (!level.isClientSide){
-                Cat cat = (Cat) EntityType.CAT.create((ServerLevel) level);
-
+                Cat cat = ocelot.convertTo(EntityType.CAT, true);
                 cat.tame(player); //this sets owner UUID
-                cat.setPos(ocelot.position());
-
                 cat.setOrderedToSit(true);
-                cat.level().broadcastEntityEvent(cat, (byte)7);
                 cat.setPersistenceRequired();
-
-                cat.finalizeSpawn((ServerLevel) level, level.getCurrentDifficultyAt(ocelot.getOnPos()), MobSpawnType.NATURAL, (SpawnGroupData)null, (CompoundTag)null);
-
-                cat.moveTo(ocelot.getOnPos().above(), ocelot.getYRot(), ocelot.getXRot()); //this is needed to set rotation for some reason. setXRot doesn't work
-
-                ((ServerLevel) level).addFreshEntityWithPassengers(cat);
-                ocelot.remove(Entity.RemovalReason.DISCARDED);
+                cat.setXRot(ocelot.getXRot());
+                //this doesn't seem to actually work, it always faces south
+                //but as far as I can tell this is only a rendering thing?
             }
         }
     }
